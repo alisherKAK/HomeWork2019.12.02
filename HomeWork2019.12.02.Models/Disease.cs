9632d614a660334cf6fbc7105b24d24cab4b0911
Alisher Kalabaev <alisher.alish333@gmail.com>
2019-12-02 20:50:25 +0600

    Initial Commit

---
+++ HomeWork2019.12.02.Models/Disease.cs
@@ -0,0 +1,16 @@
+﻿using HomeWork2019._12._02.AbstractModels;
+using System;
+
+namespace HomeWork2019._12._02.Models
+{
+    public class Disease : IEntity
+    {
+        public int Id { get; set; }
+        public string Specialist { get; set; }
+        public string SpecialistFullName { get; set; }
+        public string Diagnosis { get; set; }
+        public string Complaints { get; set; }
+        public DateTime VisitDate { get; set; }
+        public virtual Patient Patient { get; set; }
+    }
+}
